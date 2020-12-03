71c4fa3d9cdcb19c90877c38c677e45a7589140b
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-12-03 23:36:34 +0700

    Add admin role

--- src/main/java/org/example/twitter/domain/Role.java
+++ src/main/java/org/example/twitter/domain/Role.java
@@ -3,7 +3,7 @@ package org.example.twitter.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
