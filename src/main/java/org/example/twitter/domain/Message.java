879ee2575c2de8e64bb30391550506cc1a32d5e6
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-12-07 23:02:52 +0700

    Add DB migration

--- src/main/java/org/example/twitter/domain/Message.java
+++ src/main/java/org/example/twitter/domain/Message.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 public class Message {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
 
@@ -18,11 +18,11 @@ public class Message {
 
     private String filename;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
