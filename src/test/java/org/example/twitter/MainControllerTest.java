7a1a6a0efa0a4ff158f725a4d4c68982be65e974
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-12-18 22:19:02 +0700

    Finish unit tests

--- src/test/java/org/example/twitter/MainControllerTest.java
+++ src/test/java/org/example/twitter/MainControllerTest.java
@@ -6,14 +6,21 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartHttpServletRequest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
@@ -45,6 +52,34 @@ public class MainControllerTest {
                 .andDo(print())
                 .andExpect(authenticated())
                 .andExpect(xpath("//div[@id=\"message-list\"]/div").nodeCount(4));
+
+    }
+
+    @Test
+    public void filterMessageTest() throws  Exception {
+        mockMvc.perform(get("/main").param("filter", "tag"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(xpath("//div[@id=\"message-list\"]/div").nodeCount(2))
+                .andExpect(xpath("//div[@id=\"message-list\"]/div[@data-id=\"1\"]").exists())
+                .andExpect(xpath("//div[@id=\"message-list\"]/div[@data-id=\"4\"]").exists());
+    }
+
+    @Test
+    public void addMessageToListTest() throws  Exception {
+        MockHttpServletRequestBuilder multipart = multipart("/main")
+                .file("file", "testContent".getBytes())
+                .param("text", "Message text")
+                .param("tag", "test-tag")
+                .with(csrf());
+
+        mockMvc.perform(multipart)
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(xpath("//div[@id=\"message-list\"]/div").nodeCount(5))
+                .andExpect(xpath("//div[@id=\"message-list\"]/div[@data-id=\"10\"]").exists())
+                .andExpect(xpath("//div[@id=\"message-list\"]/div[@data-id=\"10\"]/div/span").string("Message text"))
+                .andExpect(xpath("//div[@id=\"message-list\"]/div[@data-id=\"10\"]/div/i").string("#test-tag"));
 
     }
 
--- src/test/resources/messages-list-before.sql
+++ src/test/resources/messages-list-before.sql
@@ -3,7 +3,7 @@ delete from message;
 insert into message(id, text, tag, user_id) values
 (1, 'first', 'tag', 1),
 (2, 'second', 'anotherTag', 1),
-(3, 'third', 'tag', 1),
+(3, 'third', 'anotherTag', 1),
 (4, 'fourth', 'tag', 1);
 
 alter sequence hibernate_sequence restart with 10;
