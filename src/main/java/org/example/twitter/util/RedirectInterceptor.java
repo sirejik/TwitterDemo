c1131556fbd9e72d4d88fe3ede7e5b6df5488a36
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-12-24 22:01:22 +0700

    Add turbolinks

---
+++ src/main/java/org/example/twitter/util/RedirectInterceptor.java
@@ -0,0 +1,19 @@
+package org.example.twitter.util;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RedirectInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null) {
+            String args = request.getQueryString() != null ? request.getQueryString() : "";
+            String url = request.getRequestURL().toString() + "?" + args;
+            response.setHeader("Turbolinks-Location", url);
+        }
+    }
+
+}
